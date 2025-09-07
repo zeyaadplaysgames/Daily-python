921511b59424b99b382d4ce73aa7094fca61567d
Zeyaad <148540407+zeyaadplaysgames@users.noreply.github.com>
2025-09-07 22:51:02 +0400

    Update Day1-number-guessing-game.py
    forgot to remove print(number) | (kinda ruins the game)

--- Day1-number-guessing-game.py
+++ Day1-number-guessing-game.py
@@ -1,11 +1,10 @@
-# for day 1 i decided to do a simple number guessing game
-
 import random
 
 number = random.randint(0, 100)
-print(number)
-guess = int(input('type in an integer from 0-100\n')) 
+# print(number) - only for debug
 
+guess = int(input('type in an integer from 0-100\n')) 
+attempts = 1 
 if guess == number:
     print('Good guess, the number was', number )
 elif guess != number:
@@ -13,8 +12,11 @@ elif guess != number:
         if guess > number:
             print('The number is lower than', guess)
             guess = int(input('type in an integer from 0-100\n')) 
+            attempts = attempts + 1
         elif guess < number:
             print('The number is higher than', guess)
             guess = int(input('type in an integer from 0-100\n'))
+            attempts = attempts + 1
     else: 
-        print('Good guess,the number was', number )
+        print('Good guess,the number was', number , 'and it took you', attempts ,'attemps')
+        print('Thanks for playing! :D')
